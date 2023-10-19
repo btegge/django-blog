fb1b032576bba0a07fd529245ec196658e44746a
Ben Tegge <benjamin.tegge@gmail.com>
2023-10-19 13:17:54 -0700

    Completed Assignment 8

--- blogging/views.py
+++ blogging/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render
-from django.http import Http404
 from blogging.models import Post
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+class BlogListView(ListView):
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    model = Post
+    template_name = 'blogging/list.html'
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class BlogDetailView(DetailView):
+    queryset = Post.objects.exclude(published_date__exact=None)
+    model = Post
+    template_name = 'blogging/detail.html'
+    # try:
+    #     post = published.get(pk=post_id)
+    # except Post.DoesNotExist:
+    #     raise Http404
+    # context = {'post': post}
+    # return render(request, 'blogging/detail.html', context)
